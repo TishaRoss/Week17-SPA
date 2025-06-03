b093b25fe7e70c3f5a0d8a9d6244e43438b7f88b
Tisha Ross <Alexandriaross1129@gmail.com>
2025-06-03 12:22:47 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//Coding Steps:
+//Using an online API of your choice, create a React project. You will be working on this for the next three weeks.
+//Project must meet the following criteria:
+//Use React Router and have at least 3 pages using React Bootstrap or an alternative styling library
+//Contain at least 10 custom components
+//Allow for all CRUD operations via one or more APIs
